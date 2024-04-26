8c9bd6bc6f33ff065bb3a4e721cc7a62c173b358
Mark Drapak <mark.d@usask.ca>
2024-04-26 01:24:58 -0600

    .

--- scripts/main.js
+++ scripts/main.js
@@ -1128,7 +1128,7 @@ $(document).ready(function() {
 
         $('.AYselector').change(function() {
             console.log('Reloading new AY...');
-            window.parent.location.href = 'https://elentra.usask.ca/community/ugmecurriculum:explorer?ay=' + this.value;
+            window.parent.location.href = '/#ay=' + this.value;
             // window.parent.location.reload();
         });
 
